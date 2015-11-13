95beb159f2472f4c5109d97e0630dd53a1be71f0
Joseph Jiao <glorysun@gmail.com>
2015-11-13 17:19:21 +0800

    add mpg123 modified version

---
+++ mpg123-modified.js
@@ -0,0 +1,79 @@
+var spawn        = require('child_process').spawn
+var EventEmitter = require('events').EventEmitter
+var inherits     = require('util').inherits
+var es           = require('event-stream')
+var through      = require('through')
+
+inherits(MpgPlayer, EventEmitter)
+
+module.exports = MpgPlayer
+function MpgPlayer() {
+  var self = this
+  this.child = spawn('mpg123', ['-R'], { stdio: ['pipe', 'pipe', null]})
+  this.stream = this.child.stdin,
+  this.child.stdout
+    .pipe(es.split())
+    .pipe(through(function (data) {
+//      console.log('data:'+ data)
+      var line = data.split(' ')
+      var type = line.shift()
+
+
+      if('@P' === type) {
+        var event = ['end', 'pause', 'resume', 'stop'][+line.shift()]
+        self.emit(event)
+      }
+      else
+      if('@E' == type) {
+        var err = new Error(line.join(' '))
+        err.type = 'mpg-player'
+        self.emit('error', err)
+      }
+      else
+      if('@F' == type) {
+        line.unshift('frame')
+        self.emit.apply(self, line)
+      }
+      
+    }))
+  this.child.stderr
+    .pipe(es.split())
+    .pipe(through(function (data) {
+    }))
+
+}
+
+var p = MpgPlayer.prototype
+
+p._cmd = function () {
+  var args = [].slice.call(arguments)
+  this.stream.write(args.join(' ') + '\n')
+  return this
+}
+
+p.play = function (file) {
+  return this._cmd('LOAD', file)
+}
+p.loadpause= function (file) {
+  return this._cmd('LP', file)
+}
+p.pause = function () {
+  return this._cmd('PAUSE')
+}
+p.stop = function () {
+  return this._cmd('STOP')
+}
+p.gain = function (vol) {
+  vol = Math.min(Math.max(Math.round(vol), 0), 100)
+  return this._cmd('GAIN', vol)
+}
+
+p.close = function () {
+  this.child.kill()
+}
+
+
+if(!module.parent) {
+  new MpgPlayer()
+    .play(process.argv[2])
+}
